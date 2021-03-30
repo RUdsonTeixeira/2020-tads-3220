a71b94f10a98379dea5390b65d155efef2a1e952
= <=>
2021-03-30 13:18:43 -0400

    aula 29/03 - DOM

---
+++ dom/js/selecionando-elementos.js
@@ -0,0 +1,16 @@
+let header = document.querySelector("header") // selecionando um único elemento
+header.style.backgroundColor="purple" // alterando o estilo do elemento selecionado
+
+let links = document.querySelectorAll("a") // selecionando vários elementos - retorna uma NodeList
+let link2 = links[1] // acessando o 2 elemento da lista
+
+link2.onmousemove = function(){ // adicionado um evento que ao passar o mouse sobre o elemento executa a função atribuída
+    link2.style.color="red"
+    link2.style.fontSize="25px"
+    console.log("movendo mouse")
+}
+link2.onclick = mensagem // adicionando o evento que ao clicar com o mouse executa a função Mensagem.
+
+function mensagem(){
+    alert("Você clicou no Link!")
+}
