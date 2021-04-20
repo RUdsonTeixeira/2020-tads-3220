5c3fbc20685222d0e5042549956b24312b40408b
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2021-04-19 21:53:05 -0400

    Resolução Lista 1 19/04

--- lista-atividades01/exercicio-02/app.js
+++ lista-atividades01/exercicio-02/app.js
@@ -2,4 +2,19 @@
    2.Elaborar um programa para uma pizzaria, o qual leia o valor total de uma conta e quantos clientes vão pagá-la. 
    Calcule e informe o valor a ser pago por cada cliente.
 */
+const form = document.querySelector('form')
+const btnFecharConta = form.querySelector('button')
+
+function fecharConta(){
+   let valorDaConta = parseFloat(form.valorDaConta.value)
+   let qtdeClientes = parseFloat(form.qtdeClientes.value)
+   let valorPorCLiente = valorDaConta/qtdeClientes
+
+   form.valorPorCliente.value = valorPorCLiente.toFixed(2)
+}
+
+btnFecharConta.addEventListener('click',fecharConta)
+
+
+
 
--- lista-atividades01/exercicio-10/app.js
+++ lista-atividades01/exercicio-10/app.js
@@ -2,4 +2,27 @@
   10.	 Crie um programa para controle das despesas onde ele deve receber como entrada do usuário a descrição e o valor da despesa que devem ser armazenados em um vetor.
    O programa deve, cada inclusão de conta, listar para o usuário as contas já cadastradas e exibir um totalizador.
 */
+const inputDescricao = document.querySelector('input[name=descricao]')
+const inputValor = document.querySelector('input[name=valor]')
+const btnAdicionar = document.querySelector('button')
+const ulDespesas = document.querySelector('.listaDespesas')
+
+let despesas = []
+function adicionarDespesa(){
+    let despesa = {
+    descricao:inputDescricao.value,
+    valor:inputValor.value
+  }
+  ulDespesas.innerHTML=''
+  inputDescricao.value=''
+  inputValor.value=''
+  inputDescricao.focus()
+  despesas.push(despesa) /* adicionando no vetor */
+  for(let i=0;i<despesas.length;i++){
+    let li = `<li>${despesas[i].descricao} - R$${despesas[i].valor}</li>`
+   ulDespesas.innerHTML+=li
+  }
+}
+btnAdicionar.addEventListener('click',adicionarDespesa)
+
 
