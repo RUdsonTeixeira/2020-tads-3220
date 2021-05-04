2a877e1c207c0b5ff80e876373973bba3add0c54
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2021-05-03 21:01:07 -0400

    update app-lista-tarefas

--- atividade-projetos/app-lista-tarefas/js/app.js
+++ atividade-projetos/app-lista-tarefas/js/app.js
@@ -4,11 +4,38 @@ const tasksList = document.querySelector('.tasks-list')
 
 function addTask(){
     let li = document.createElement('li')
-    li.textContent= inputTask.value
     li.className='task'
+
+    li.addEventListener('click',function(event){
+        taskDelete(event)
+})
+
+    let divTaskTitle = document.createElement('div')
+    divTaskTitle.className ='task-title'
+    divTaskTitle.textContent=inputTask.value
+
+    let divTaskActions = document.createElement('div')
+    divTaskActions.className='task-actions'
+
+    let btnExcluir = document.createElement('button')
+    btnExcluir.className='btn btn-danger'
+    btnExcluir.innerHTML= '<i class="bi bi-trash-fill"></i>'
+   
+
+    divTaskActions.appendChild(btnExcluir)
+
+    li.appendChild(divTaskTitle)
+    li.appendChild(divTaskActions)
+
     tasksList.appendChild(li)
     inputTask.value=''
     inputTask.focus()
 
+}
+function taskDelete(event){
+    let element = event.target
+    if(element.tagName=='BUTTON'){
+
+    }    
 }
 btnAdd.addEventListener('click',addTask)
